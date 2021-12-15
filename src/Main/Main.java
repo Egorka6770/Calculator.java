5c5c66465c07f9e5b9754ba3a1548b08154b8b94
Egorka6770 <74188157+Egorka6770@users.noreply.github.com>
2021-12-15 09:31:20 +0300

    Main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         double x;
-        String primer = "16 / ( 0 + 0 ) ";
+        String primer = "2 * 9";
         Calculator calc = new Calculator();
         x = calc.rechenie(primer);
         System.out.println(x);
