aab6a2c8d1e7ab65da2272d0a427666e189fe732
Egorka6770 <74188157+Egorka6770@users.noreply.github.com>
2021-12-22 23:10:26 +0300

    22

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,10 +1,15 @@
 package Main;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         double x;
-        String primer = "sqrt 2 ( 4 ^ 2 / 2 * 4 + 32 ) - 6";
+        String primer;
+        System.out.println("Введите пример:");
+        Scanner scanner = new Scanner(System.in);
+        primer = scanner.nextLine();
         Calculator calc = new Calculator();
         x = calc.rechenie(primer);
         System.out.print(x);
